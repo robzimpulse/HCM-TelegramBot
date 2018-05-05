b4c77a15dd7d2cadffcf1303a35b5f8c74314a80
Robyarta Haruli Ruci <kugelfang.killaruna@gmail.com>
2018-05-05 11:28:02 +0700

    fixing email auth

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const token = '560008768:AAFAouOsURJYbt51HdqC7lFSdbFfycMMYgw';
-const clientId = '478647044238-l1sfkd0h0ucf1a3lro374fjtssgetjmd.apps.googleusercontent.com';
-const clientSecret = 'YeUTQEMP_Fi0XggWeTq6FsPf';
-
+const logic = require('./logic');
 const sha1 = require('sha1');
 const createError = require('http-errors');
 const express = require('express');
@@ -9,67 +6,25 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
-const request = require('request-promise');
 const Telegraf = require('telegraf');
-const Markup = require('telegraf/markup');
 const commandParts = require('telegraf-command-parts');
-const admin = require('firebase-admin');
-const serviceAccount = require('./hcm-telegrambot-a6db72963a37.json');
-const { google }= require('googleapis');
-const scope = ['https://www.googleapis.com/auth/userinfo.email'];
-const oauth2Client = new google.auth.OAuth2(clientId,clientSecret,'http://localhost:3000/auth');
-const url = oauth2Client.generateAuthUrl({access_type: 'offline', scope: scope});
-const authButton = Markup.inlineKeyboard([ Markup.urlButton('Authorize️', url)]).extra();
-const bot = new Telegraf(token);
+
+const bot = new Telegraf(process.env.TOKEN);
 const app = express();
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://hcm-telegrambot.firebaseio.com'
-});
-
-const startText = "Hello, Ada yang bisa aku bantu?";
-
-const updateChatIDWithUsername = (ctx, next) => {
-    console.log("save chat_id: " + ctx.message.chat.id + " to username @"+ctx.message.chat.username);
-    admin.database().ref('users')
-        .child(ctx.message.chat.id)
-        .update({ username: "@"+ctx.message.chat.username })
-        .catch( () => { console.log("Error updating chat_id") });
-
-    const command = ctx.state.command;
-
-    if (command.command !== 'start' || command.splitArgs[0].length < 1) { return next() }
-
-    admin.database().ref('auth')
-        .child(command.splitArgs[0])
-        .on('value', snapshot => {
-            oauth2Client.getToken(snapshot.val().code, (err, token) => {
-                console.log(err);
-                if (err) { return }
-                oauth2Client.credentials = token;
-                google
-                    .plus({version: 'v1', auth: oauth2Client})
-                    .people.get({ userId: 'me' }, (err, response) => {
-                    console.log(err);
-                    admin.database().ref('users')
-                        .child(ctx.message.chat.id)
-                        .update({ email: response.data.emails[0].value })
-                        .catch( () => { console.log("Error updating email") });
-                });
-            });
-        });
-
-    next()
-};
 
 // bot setup
 bot.use(commandParts());
-bot.catch( err => { console.log('Error: ', err) });
-bot.start(updateChatIDWithUsername, ctx => ctx.replyWithHTML(startText));
-bot.command('update_email', updateChatIDWithUsername, ctx => {
-    return ctx.reply("Mohon login menggunakan button dibawah ini", authButton)
-});
+bot.catch(logic.error);
+bot.start(
+    logic.triggerSaveUsername,
+    logic.triggerAuthorizeEmail,
+    logic.triggerUpdateEmail,
+    logic.greeting
+);
+
+// bot.command('update_email', logic.updateUsername, ctx => {
+//     return ctx.reply("Mohon login menggunakan button dibawah ini", authButton)
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -84,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.all('/auth', function (req, res) {
     const hash = sha1(req.query.code);
-    admin.database().ref('auth').child(hash).set({ code: req.query.code });
+    logic.createHash(hash, req.query.code);
     res.redirect('http://t.me/bl_hcm_bot?start='+hash);
 });
 
---
+++ logic.js
@@ -0,0 +1,112 @@
+// /**
+//  * Created by robyarta on 04/05/18.
+//  */
+
+const request = require('request-promise');
+const admin = require('firebase-admin');
+const serviceAccount = require('./hcm-telegrambot-a6db72963a37.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://hcm-telegrambot.firebaseio.com'
+});
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const Markup = require('telegraf/markup');
+const { google }= require('googleapis');
+const oauth2Client = new google.auth.OAuth2(clientId,clientSecret,'http://localhost:3000/auth');
+const scope = ['https://www.googleapis.com/auth/userinfo.email'];
+const url = oauth2Client.generateAuthUrl({access_type: 'offline', scope: scope});
+const authButton = Markup.inlineKeyboard([ Markup.urlButton('Authorize️', url)]).extra();
+
+const Telegram = require('telegraf/telegram');
+const api = new Telegram(process.env.TOKEN);
+
+const emailUpdateSuccess = "Sukses menambahkan email kakak";
+const emailNotFoundText = "Mohon maaf, Email kakak setelah di cek masih kosong. Mohon login yak.";
+const startText = "Hello, Ada yang bisa aku bantu?";
+const responseText = "Baik kak, Keluhannya akan kami tindak lanjuti";
+
+const getEmail = (token, next) => {
+    return oauth2Client.getToken(token, (err, credentials) => {
+        if (err) { return next(null); }
+        oauth2Client.credentials = credentials;
+        google.plus({version: 'v1', auth: oauth2Client}).people.get({ userId: 'me' }, (err, response) => {
+            if (err) { return next(null); }
+            return next(response.data.emails[0].value);
+        });
+    });
+};
+
+module.exports =  {
+
+    createHash: (hash, code) => { admin.database().ref('auth').child(hash).set({ code: code }); },
+
+    greeting: ctx => ctx.replyWithHTML(startText),
+
+    triggerSaveUsername: (ctx, next) => {
+        console.log("triggerSaveUsername: "+ctx.message.chat.id+" - @"+ctx.message.chat.username);
+        admin.database().ref('users').child(ctx.message.chat.id).update({ username: "@"+ctx.message.chat.username });
+        return next()
+    },
+
+    triggerAuthorizeEmail: (ctx, next) => {
+        console.log("triggerAuthorizeEmail: "+ctx.message.chat.id+" - @"+ctx.message.chat.username);
+        setTimeout(() => {
+            admin.database().ref('users').child(ctx.message.chat.id).once('value').then(snapshot => {
+                if (snapshot.hasChild('email')) {return}
+                ctx.replyWithHTML(emailNotFoundText, authButton);
+            });
+        }, 1000);
+        return next();
+    },
+
+    triggerUpdateEmail: (ctx, next) => {
+        console.log("triggerUpdateEmail: "+ctx.message.chat.id+" - @"+ctx.message.chat.username);
+        const command = ctx.state.command;
+        if (command.command !== 'start' || command.splitArgs[0].length < 1) { return next(); }
+        admin.database().ref('auth').child(command.splitArgs[0]).once('value').then(snapshot => {
+            getEmail(snapshot.val().code, email => {
+                admin.database().ref('auth').child(command.splitArgs[0]).remove();
+                admin.database().ref('users').child(ctx.message.chat.id).update({ email: email })
+                    .then(() => ctx.replyWithHTML(emailUpdateSuccess + " <b>"+email+"</b>"))
+            })
+        });
+        return next()
+    },
+
+};
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// ctx.telegram.sendMessage(ctx.message.chat.id, url);
+//
+// const helpText = "Berikut adalah command yang bisa digunakan: \n\n" +
+//              "-\/help (Bantuan)"+
+//              "-\/complain (Submit Complain)\n" +
+//              "-\/profile (Update Data Pegawai)\n";
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// bot.use(Telegraf.log());
+// bot.help(updateChatIDWithUsername, ctx => ctx.replyWithHTML(helpText));
+//
+//
+//
